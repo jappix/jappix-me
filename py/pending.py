e8baddaf1fe3056cdece199442c0aef27634496f
Valérian Saliou <val.saliou@gmail.com>
2013-06-03 23:21:35 +0200

    No more debug, stable :)

--- py/pending.py
+++ py/pending.py
@@ -323,7 +323,7 @@ def login(user, domain, pwd):
 		'success': False
 	}
 
-	con = xmpp.Client(domain)
+	con = xmpp.Client(domain, debug=[])
 	connector = con.connect(server=(domain, 5222))
 	
 	if connector and con.auth(user, pwd, 'Jappix Me (PB' + str(int(time.time())) + ')'):
