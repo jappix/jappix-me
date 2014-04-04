de5284364228c0653e7dfe35bfb492c7c76f0066
Valérian Saliou <valerian@valeriansaliou.name>
2014-04-04 21:58:08 +0200

    URLs update, round 2

--- index.php
+++ index.php
@@ -404,7 +404,7 @@ if($page == '404') {
 	
 	<?php if(($page == 'home') || ($page == 'user') || ($page == 'new') || ($page == 'privacy')) { ?>
 	<div id="foot" class="wrapped">
-		<span><b><a href="https://project.jappix.com/contact">Contact</a></b> - <b><a href="https://legal.jappix.com/">Legal</a></b> - © 2011-<?php echo(date('Y')); ?> <a href="http://frenchtouch.pro/">FrenchTouch Web Agency</a></span>
+		<span><b><a href="https://project.jappix.com/contact">Contact</a></b> - <b><a href="https://legal.jappix.com/">Legal</a></b> - © 2011-<?php echo(date('Y')); ?> <a href="https://frenchtouch.pro/">FrenchTouch Web Agency</a></span>
 		<?php if($page == 'user') {
 			$google_link = 'https://www.google.com/search?q='.rawurlencode('site:'.$path_root_domain.'/'.$user);
 			
